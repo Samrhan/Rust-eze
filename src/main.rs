c86bab4faacbf30165afdcbb723b8d2e14cfb480
Samuel BADER <samuel.bader83@gmail.com>
2024-11-12 22:49:50 +0100

    refactored

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,33 @@ use std::net::TcpListener;
 use std::thread;
 
 fn send_response(mut stream: std::net::TcpStream, response: &str) {
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 fn handle_client(mut stream: std::net::TcpStream) {
     let mut buffer = [0; 512];
+
     loop {
         match stream.read(&mut buffer) {
-            Ok(0) => break,
+            Ok(0) => break, // Connection closed
             Ok(_) => {
                 let request = String::from_utf8_lossy(&buffer);
-                let parts: Vec<&str> = request.split("\r\n").collect();
-                if parts.len() > 4 && parts[2].eq_ignore_ascii_case("ECHO") {
-                    let message = parts[4];
-                    let response = format!("${}\r\n{}\r\n", message.len(), message);
-                    send_response(stream.try_clone().unwrap(), &response);
+                let mut parts = request.split("\r\n");
+
+                if let (Some(_), Some(_), Some(command), Some(_), Some(message), ..) = (parts.next(), parts.next(), parts.next(), parts.next(), parts.next()) {
+                    if command.eq_ignore_ascii_case("ECHO") {
+                        let response = format!("${}\r\n{}\r\n", message.len(), message);
+                        send_response(stream.try_clone().unwrap(), &response);
+                    } else {
+                        send_response(stream.try_clone().unwrap(), "+PONG\r\n");
+                    }
                 } else {
-                    send_response(stream.try_clone().unwrap(), "+PONG\r\n");
+                    send_response(stream.try_clone().unwrap(), "-ERR Invalid request\r\n");
                 }
             }
             Err(e) => {
-                println!("error reading from stream: {}", e);
+                println!("Error reading from stream: {}", e);
                 break;
             }
         }
@@ -37,13 +42,13 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
+                println!("Accepted new connection");
                 thread::spawn(move || {
                     handle_client(stream);
                 });
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting connection: {}", e);
             }
         }
     }
