6a4dda597b9ebb61d6decfd0e143951ad272b2ef
Samuel BADER <samuel.bader83@gmail.com>
2024-11-12 22:44:56 +0100

    Handle multiple client

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
 
 fn send_pong_response(mut stream: std::net::TcpStream) {
     let response = "+PONG\r\n";
@@ -7,26 +8,32 @@ fn send_pong_response(mut stream: std::net::TcpStream) {
     stream.flush().unwrap();
 }
 
+fn handle_client(mut stream: std::net::TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => break, 
+            Ok(_) => {
+                send_pong_response(stream.try_clone().unwrap());
+            }
+            Err(e) => {
+                println!("error reading from stream: {}", e);
+                break;
+            }
+        }
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 512];
-                loop {
-                    match stream.read(&mut buffer) {
-                        Ok(0) => break, // Connection closed
-                        Ok(_) => {
-                            send_pong_response(stream.try_clone().unwrap());
-                        }
-                        Err(e) => {
-                            println!("error reading from stream: {}", e);
-                            break;
-                        }
-                    }
-                }
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
