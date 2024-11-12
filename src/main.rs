c0f72aa09b4025305c98aab741a7d7565ab7e0d2
Samuel BADER <samuel.bader83@gmail.com>
2024-11-12 22:47:04 +0100

    Handle ECHO command

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::thread;
 
-fn send_pong_response(mut stream: std::net::TcpStream) {
-    let response = "+PONG\r\n";
+fn send_response(mut stream: std::net::TcpStream, response: &str) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
@@ -12,9 +11,17 @@ fn handle_client(mut stream: std::net::TcpStream) {
     let mut buffer = [0; 512];
     loop {
         match stream.read(&mut buffer) {
-            Ok(0) => break, 
+            Ok(0) => break,
             Ok(_) => {
-                send_pong_response(stream.try_clone().unwrap());
+                let request = String::from_utf8_lossy(&buffer);
+                let parts: Vec<&str> = request.split("\r\n").collect();
+                if parts.len() > 4 && parts[2].eq_ignore_ascii_case("ECHO") {
+                    let message = parts[4];
+                    let response = format!("${}\r\n{}\r\n", message.len(), message);
+                    send_response(stream.try_clone().unwrap(), &response);
+                } else {
+                    send_response(stream.try_clone().unwrap(), "+PONG\r\n");
+                }
             }
             Err(e) => {
                 println!("error reading from stream: {}", e);
